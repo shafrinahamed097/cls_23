4941e576c98884b2a91e253040831ee8ca1f861a
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-07-08 20:41:01 +0600

    Added 2 funciton and connect with route

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
-    //
+    // Response Type String
+
+    function demo1(){
+        return "Response Type String";
+    }
+
+    // Response Type Int
+
+    function demo2(){
+        return 1;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\DemoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/demo1', [DemoController::class,'demo1']);
+Route::get('/demo2', [DemoController::class,'demo2']);
+
+
+
