92afd9418d90c50bc7e86c523c80b7fdbb453339
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-07-09 11:20:50 +0600

    Declare Array & Associative Array -> Connected in route. N.T.: Associative Array Automatically Convert in json in Laravel

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -15,6 +15,30 @@ class DemoController extends Controller
     // Response Type Int
 
     function demo2(){
-        return 1;
+        return 100;
     }
+
+    // Response Type Null
+    function demo3(){
+        return null;
+    }
+
+    // Response Type Boolean
+    function demo4(){
+        return true;
+    }
+
+    // Response Type Array
+
+    function demo5(){
+        return array('A', 'B', 'C');
+    }
+
+    // Response Type Associative Array
+
+    function  demo6(){
+        return array(['name'=>'ostad', 'age'=>20], ['name'=>'ostad1', 'age'=>30]);
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/demo1', [DemoController::class,'demo1']);
 Route::get('/demo2', [DemoController::class,'demo2']);
+Route::get('/demo3', [DemoController::class,'demo3']);
+Route::get('/demo4', [DemoController::class,'demo4']);
+Route::get('/demo5', [DemoController::class,'demo5']);
+Route::get('/demo6', [DemoController::class,'demo6']);
 
 
 
